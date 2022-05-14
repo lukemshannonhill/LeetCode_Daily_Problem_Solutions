a5def2ca07af89202be6e0be2bd4b0576d1de9bc
Luke Shannon-Hill <lukemshannonhill@gmail.com>
2022-05-14 11:45:48 -0700

    Update fibonacci_number.py

--- Easy/fibonacci_number.py
+++ Easy/fibonacci_number.py
@@ -19,3 +19,13 @@ class Solution:
             
         return a[i]
         
+# reattempt, 5/14/22:
+'''
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        if n <= 1:
+            return n
+        else:
+            return self.fib(n-1) + self.fib(n-2)
+'''
